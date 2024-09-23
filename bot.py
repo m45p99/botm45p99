eef564eb86c35bc95e617adffd7d86f6cb30016a
m45p99 <109224038+m45p99@users.noreply.github.com>
2024-09-23 15:59:27 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -26,7 +26,10 @@ def coin_handler(message):
 
 
 
-
+@bot.message_handler(content_types=['new_chat_members'])
+def make_some(message):
+    bot.send_message(message.chat.id, 'I accepted a new user!')
+    bot.approve_chat_join_request(message.chat.id, message.from_user.id)
 
 
 
